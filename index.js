93de45a19c317cdc505f4d2324bfb539bdf9b102
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-04 17:46:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var incMe = document.querySelectorAll ('.ranked-list li')
+  for (let i=0; i<incMe.length; i++) {
+    incMe[i].innerHTML = parseInt(incMe[i].innerHTML)+n
+  }
+  return incMe
+}
+
+function deepestChild() {
+  var dc = document.querySelector('#grand-node').querySelectorAll('div')
+  return dc[dc.length-1]
+}
+
